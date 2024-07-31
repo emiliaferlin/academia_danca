896cb87e54e3ff5c02cd2768dfac8937b118b36a
Emilia <emilia.ferlin@sbsistemas.com.br>
2024-07-31 10:29:11 -0300

    refactor: ajusta confi

--- src/main/java/br/edu/ifsul/cc/lpoo/academia_danca/dao/PersistenciaJPA.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/academia_danca/dao/PersistenciaJPA.java
@@ -11,6 +11,7 @@ import br.edu.ifsul.cc.lpoo.academia_danca.model.Modalidade;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 
 
@@ -22,108 +23,56 @@ import javax.persistence.Persistence;
  */
 public class PersistenciaJPA implements InterfacePersistencia{
     
-    public EntityManagerFactory factory;    //fabrica de gerenciadores de entidades
-    public EntityManager entity;            //gerenciador de entidades JPA
-
-    public PersistenciaJPA() {
-        //parametro: é o nome da unidade de persistencia (Persistence Unit)
-        factory = Persistence.createEntityManagerFactory("pu_sistema_compras");
-        //conecta no bd e executa a estratégia de geração.
-        entity = factory.createEntityManager();
+    EntityManagerFactory factory; //fabrica de gerenciadores de entidades
+    EntityManager entity; //gerenciador de entidades JPA
+    
+    public PersistenciaJPA(){
+        //parametro: é o nome da unidade de persistencia
+        factory = Persistence.createEntityManagerFactory("pu_academia_danca");
+        entity = factory.createEntityManager();    // estabelece a conexão com o banco de dados e executa a estratégia de geração
     }
 
     @Override
     public Boolean conexaoAberta() {
-        if (entity == null || !entity.isOpen()) {
-            entity = factory.createEntityManager();
-        }
         return entity.isOpen();
     }
 
     @Override
     public void fecharConexao() {
-        if (entity != null && entity.isOpen()) {
-            entity.close();
-        }
+        entity.close(); 
     }
 
     @Override
     public Object find(Class c, Object id) throws Exception {
-        EntityManager em = getEntityManager();
-        return em.find(c, id);//encontra um determinado registro 
+       
+        return entity.find(c, id);//encontra um determinado registro              
     }
 
-    public void persist(Object o) {
-        EntityManager em = getEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.persist(o);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-            throw e;
-        }
-    }
-
-    /*
-    Todos os métodos agora chamam getEntityManager() para garantir que o EntityManager esteja sempre aberto e pronto para uso.
-     */
-    public EntityManager getEntityManager() {
-        if (entity == null || !entity.isOpen()) {
-            entity = factory.createEntityManager();
-        }
-        return entity;
+    @Override
+    public void persist(Object o) throws Exception {
+        
+        entity.getTransaction().begin();// abrir a transacao.
+        entity.persist(o); //realiza o insert ou update.
+        entity.getTransaction().commit(); //comita a transacao (comando sql)                
     }
 
     @Override
     public void remover(Object o) throws Exception {
-        //No método remover, antes de chamar remove, usamos merge se o objeto não estiver gerenciado.
-        EntityManager em = getEntityManager();
-        try {
-            em.getTransaction().begin();
-            if (!em.contains(o)) {
-                o = em.merge(o); // Anexa o objeto ao contexto de persistência, se necessário
-            }
-            em.remove(o);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-            throw e;
-        }
+        
+        entity.getTransaction().begin();// abrir a transacao.
+        entity.remove(o); //realiza o delete
+        entity.getTransaction().commit(); //comita a transacao (comando sql)                
+    }
+    
+    public void atualiza(Object o) throws Exception {
+        entity.getTransaction().begin();
+        entity.merge(o);
+        entity.getTransaction().commit();
+       
     }
     
     public List<Modalidade> getModalidades() {
-         EntityManager em = getEntityManager();
-        try {
-            return em.createNamedQuery("Modalidade.orderbyid").getResultList();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }    
-    
-    
-    public void atualiza(Object o) throws Exception {
-         //No método remover, antes de chamar remove, usamos merge se o objeto não estiver gerenciado.
-        EntityManager em = getEntityManager();
-        try {
-            em.getTransaction().begin();
-            if (!em.contains(o)) {
-                o = em.merge(o); // Anexa o objeto ao contexto de persistência, se necessário
-            }
-            em.merge(o);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-            throw e;
-        }
-       
+        return entity.createNamedQuery("Modalidade.orderbyid").getResultList();
     }
     
     
